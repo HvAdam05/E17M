783b72a53bcaf23988667f7f161e71e8b6f61622
horvat.adam <horvat.adam@28-15>
2023-03-02 10:45:54 +0100

    Add project files.

---
+++ txt2srt/IdozitettFelirat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace txt2srt
+{
+    internal class IdozitettFelirat
+    {
+        public string Idozites;
+        public string Felirat;
+        public IdozitettFelirat(string idozites, string felirat)
+        {
+               this.Idozites = idozites;
+               this.Felirat = felirat;
+        }
+
+        public int SzavakSzama
+        {
+            get { return Felirat.Split(' ').Count(); }
+        }
+        public string SrtIdozites
+        {
+            get
+            {
+                var ti = Idozites.Split(" - ");
+                var top = ti[0].Split(":");
+                var iop = ti[1].Split(":");
+                TimeSpan tol = new(
+                    hours: 0,
+                    minutes: int.Parse(top[0]),
+                    seconds: int.Parse(top[1]));
+                TimeSpan ig = new(
+                    hours: 0,
+                    minutes: int.Parse(iop[0]),
+                    seconds: int.Parse(iop[1]));
+                return $"{tol} ---> {ig}";
+            }
+        }
+
+    }
+}
---
+++ txt2srt/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace txt2srt
+{
+    internal class Program
+    {
+        public static List<IdozitettFelirat> IdoFelirat_ls = new();
+
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("..\\..\\..\\src\\feliratok.txt", FileMode.Open);
+            var sr = new StreamReader(fs);
+            while (!sr.EndOfStream)
+            {
+                IdoFelirat_ls.Add(new IdozitettFelirat(sr.ReadLine(), sr.ReadLine()));
+            }
+            fs.Close();
+            sr.Close();
+            Console.WriteLine($"5. feladat - Feliratok száma: {IdoFelirat_ls.Count}");
+            int maxszavak = int.MinValue;
+            string maxfelirat = "";
+            for (int i = 0; i < IdoFelirat_ls.Count; i++)
+            {
+                if (maxszavak < IdoFelirat_ls[i].SzavakSzama)
+                {
+                    maxszavak = IdoFelirat_ls[i].SzavakSzama;
+                    maxfelirat = IdoFelirat_ls[i].Felirat;
+                }
+            }
+            Console.WriteLine($"7. feladat - Legtöbb szóból álló felirat:\n{maxfelirat}"); ;
+            FileStream fs2 = new FileStream(@"felirat.srt", FileMode.Create);
+            var sw = new StreamWriter(fs2);
+            int sz = 0;
+            foreach (var i in IdoFelirat_ls)
+            {
+                sz++;
+                sw.WriteLine($"{sz}\n{i.SrtIdozites}\n{i.Felirat}\n");
+            }
+            sw.Close();
+            fs2.Close();
+        }
+    }
+}
